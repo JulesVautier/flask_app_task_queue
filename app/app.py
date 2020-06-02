09d328eb0ebfceec7d5f9c9d43e48b06311cb3fb
Jules Vautier <jules.vautier@epitech.eu>
2020-06-02 20:37:35 +0200

    Post endpoint

--- app/app.py
+++ app/app.py
@@ -1,15 +1,15 @@
 # app.py - a minimal flask api using flask_restful
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-api.add_resource(HelloWorld, '/')
+@app.route('/',  methods=['POST'])
+def index():
+    if request.method == 'POST':
+        return request.get_json()['text']
+    return "OK"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
