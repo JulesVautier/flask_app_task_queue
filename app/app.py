de3d363f04db45ed81a55ccef2ab516163e70c9b
Jules Vautier <jules.vautier@epitech.eu>
2020-06-02 20:41:26 +0200

    use status

--- app/app.py
+++ app/app.py
@@ -1,15 +1,14 @@
 # app.py - a minimal flask api using flask_restful
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_api import status
 
 app = Flask(__name__)
 api = Api(app)
 
 @app.route('/',  methods=['POST'])
-def index():
-    if request.method == 'POST':
-        return request.get_json()['text']
-    return "OK"
+def post():
+    return request.get_json()['text'], status.HTTP_200_OK
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
