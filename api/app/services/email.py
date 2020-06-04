8e00465dd426328625032dd4962ef69afc1147b4
Jules Vautier <jules.vautier@epitech.eu>
2020-06-04 14:13:24 +0200

    add email service

--- api/app/services/email.py
+++ api/app/services/email.py
@@ -1,5 +1,17 @@
-from api.app.services.service_types import ServiceTypes
-from api.app.services.services import Service
+from flask import Flask
+
+from .service_types import ServiceTypes
+from .services import Service
+
+from flask_mail import Mail, Message
+import os
+
+app_flask = Flask(__name__)
+
+
+
+app_flask.config.update(mail_settings)
+mail = Mail(app_flask)
 
 class EmailService(Service):
     service_type = ServiceTypes.EMAIL
--- api/app/worker.py
+++ api/app/worker.py
@@ -1,7 +1,10 @@
 import sys
 
+from app.services.email import EmailService
+
 
 def task_handler(args):
+    EmailService().send_data(args)
     # this function takes around 2 minutes to complete
     print('test {}'.format(args), file=sys.stderr)
     return True
