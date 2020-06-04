92377cdfbd77cdd7fbf3c427ea2a8141c7b3738b
Jules Vautier <jules.vautier@epitech.eu>
2020-06-04 13:59:47 +0200

    remove routes bc it's useless

--- api/app/api.py
+++ api/app/api.py
@@ -1,12 +1,18 @@
 import rq
 from flask import Flask
-from flask_restful import Api
+from flask import request
+from flask_api import status
 
 from redis import Redis
 
-from app.routes import simple_blueprint
+
+from app.worker import task_handler
 
 app_flask = Flask(__name__)
-app_flask.register_blueprint(simple_blueprint)
 redis = Redis(host='redis', port=6379)
 q = rq.Queue(connection=redis)
+
+@app_flask.route('/', methods=['POST'])
+def post():
+    job = q.enqueue(task_handler, None)
+    return request.get_json()['text'], status.HTTP_200_OK
--- workers/settings.py
+++ workers/settings.py
@@ -1,9 +1,5 @@
 REDIS_HOST = 'redis'
 REDIS_PORT = 6379
 
-# You can also specify the Redis DB to use
-# REDIS_DB = 3
-# REDIS_PASSWORD = 'very secret'
-
 # Queues to listen on
 QUEUES = ['default']
